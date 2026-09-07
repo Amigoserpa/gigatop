42da2688eec61923a13c39c2b36aa344c227f124
Gigatop Creative OS <creative-os@local>
2026-09-07 13:30:52 +0200

    feat: add hero images with compression (6 images, ~90% size reduction)

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { HomepageV3 } from './v3/homepage-v3';
-
-export default function RootPage() {
-  return <HomepageV3 />;
-}
--- app/robots.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: '*', allow: '/', disallow: ['/v3', '/v3/compare'] },
-    sitemap: 'https://gigatop.io/sitemap.xml',
-  };
-}
